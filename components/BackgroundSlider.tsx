e7c5bd330677589eb97902aa745def8ad6098a53
Enkhtushig <enkhtushigee@gmail.com>
2026-03-12 12:00:58 +0800

    moved bg picture down to 1 %

--- components/BackgroundSlider.tsx
+++ components/BackgroundSlider.tsx
@@ -33,7 +33,7 @@ export default function BackgroundSlider() {
             alt={`Background slide ${index + 1}`}
             fill
             className="object-cover"
-            style={{ objectPosition: "center 10%" }}
+            style={{ objectPosition: "center 1%" }}
             priority={index === 0}
             quality={90}
           />
